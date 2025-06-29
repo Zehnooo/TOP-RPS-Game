091b4619efad3488d18188ef1605968ecfd0a28a
ZehnoHome <126801501+Zehnooo@users.noreply.github.com>
2025-06-28 22:49:56 -0500

    created playGame function that loops playround until someone hits the winning score (5).

--- script.js
+++ script.js
@@ -1,79 +1,102 @@
 console.log("hello wall!");
 
-let choices = ['rock', 'paper', 'scissors'];
+let choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 let computerChoice;
 let humanChoice;
 
 function showScoreUpdate() {
-    console.log(
-        "Round completed! The new scores are: \n User Score: " + playerScore + " " + "Computer score: " + computerScore + "."
-    );
+  console.log(
+    "Round completed! The new scores are: \n User Score: " +
+      playerScore +
+      " " +
+      "Computer score: " +
+      computerScore +
+      "."
+  );
 }
 
 function showPlayerLose(humanChoice, computerChoice) {
-    console.log("Nice try! User loses!");
-        console.log(
-            "user choice was: " + humanChoice + " " +
-            "computer choice was: " + computerChoice
-        );
+  console.log("Nice try! User loses!");
+  console.log(
+    "user choice was: " +
+      humanChoice +
+      " " +
+      "computer choice was: " +
+      computerChoice
+  );
 }
 
 function showPlayerWin(humanChoice, computerChoice) {
-    console.log("Congrats! User wins!");
-        console.log(
-            "user choice was: " + humanChoice + " " +
-            "computer choice was: " + computerChoice
-        );
+  console.log("Congrats! User wins!");
+  console.log(
+    "user choice was: " +
+      humanChoice +
+      " " +
+      "computer choice was: " +
+      computerChoice
+  );
 }
+
 function getComputerChoice() {
-    const randNum = Math.floor(Math.random() * 3);
-    const computerChoice = choices[randNum];
-    return computerChoice;
+  const randNum = Math.floor(Math.random() * 3);
+  const computerChoice = choices[randNum];
+  return computerChoice;
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Enter 'rock', 'paper', or 'scissors'!");
-    humanChoice = humanChoice.trim().toLowerCase();
-    return humanChoice;
+  let humanChoice = prompt("Enter 'rock', 'paper', or 'scissors'!");
+  humanChoice = humanChoice.trim().toLowerCase();
+  return humanChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
-    
-    // Tie round
-    if (humanChoice === computerChoice) {
-        alert("Tie! No points awarded");
-          console.log(
-            "player choice was: " + humanChoice + " " +
-            "computer choice was: " + computerChoice
-        );
-    }
+  // Tie round
+  if (humanChoice === computerChoice) {
+    alert("Tie! No points awarded");
+    console.log(
+      "player choice was: " +
+        humanChoice +
+        " " +
+        "computer choice was: " +
+        computerChoice
+    );
+  }
 
-    // Player Wins
-    if ((humanChoice === 'rock' && computerChoice === 'scissors')||
-        (humanChoice === 'paper' && computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper')){
-        showPlayerWin(humanChoice, computerChoice);
-        playerScore++;
-        showScoreUpdate();
-    }
-    
-    // Player loses
-     if ((humanChoice === 'scissors' && computerChoice === 'rock')||
-        (humanChoice === 'rock' && computerChoice === 'paper') ||
-        (humanChoice === 'paper' && computerChoice === 'scissors')){
-         showPlayerLose(humanChoice, computerChoice);
-         computerScore++
-         showScoreUpdate();
-    }
+  // Player Wins
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    showPlayerWin(humanChoice, computerChoice);
+    playerScore++;
+    showScoreUpdate();
+  }
+
+  // Player loses
+  if (
+    (humanChoice === "scissors" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors")
+  ) {
+    showPlayerLose(humanChoice, computerChoice);
+    computerScore++;
+    showScoreUpdate();
+  }
 }
 
 function playGame() {
-    
+  while (playerScore < 5 || computerScore < 5) {
+    if (playerScore === 5 || computerScore === 5) {
+      break;
+    }
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+  }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
