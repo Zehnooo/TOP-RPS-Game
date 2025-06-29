9e886078c80736e0fc53d32bffacdc796c55d335
ZehnoHome <126801501+Zehnooo@users.noreply.github.com>
2025-06-28 21:51:46 -0500

    got user input and started the play round logic

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 console.log("hello wall!");
 
 let choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+let computerChoice;
+let humanChoice;
 
 function getComputerChoice() {
     const randNum = Math.floor(Math.random() * 3);
     const computerChoice = choices[randNum];
-    console.log(computerChoice);
+    return computerChoice;
 }
 
-getComputerChoice();
+function getHumanChoice() {
+    let humanChoice = prompt("Enter 'rock', 'paper', or 'scissors'!");
+    humanChoice = humanChoice.trim().toLowerCase();
+    return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    const humanChoiceGame = getComputerChoice();
+    const computerChoiceGame = getHumanChoice();
+
+    if (humanChoiceGame === computerChoiceGame) {
+        alert("Tie! No points awarded");
+    }
+}
+
+playRound();
