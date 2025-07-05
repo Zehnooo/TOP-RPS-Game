11423bbbed2df5fee70ef9a92b0883e56ba78652
ZehnoHome <126801501+Zehnooo@users.noreply.github.com>
2025-07-05 18:41:55 -0500

    reworked the entire game.. didn't commit very well because I was tunnel visioned.. whoops

--- script.js
+++ script.js
@@ -1,68 +1,35 @@
+window.addEventListener("DOMContentLoaded", () => {
+  body = document.querySelector("body");
+  body.style.opacity = "1";
+});
+
 let choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 let computerChoice;
 let humanChoice;
+let roundNum = 0;
+let tieCount = 0;
+let resetBtn;
+
+const gameButtonsDiv = document.querySelector(".game-choices");
+let gameButtons = gameButtonsDiv.querySelectorAll("button");
+
+const scoreBoard = document.querySelector(".scoreboard");
+let scoreNums = scoreBoard.querySelectorAll("span");
+
+const gameResults = document.querySelector(".game-results");
+let gameResultNums = gameResults.querySelectorAll("span");
+let gameResultText = gameResults.querySelector("p");
+
+let matchResultText = document.querySelector(".result-text");
 
 console.log("Script is running");
 
 function showScoreUpdate() {
-  console.log(
-    "Round completed! The new scores are: \n User Score: " +
-      playerScore +
-      " " +
-      "Computer score: " +
-      computerScore +
-      "."
-  );
-}
-
-function resetGame() {
-  let playAgain = confirm("Play again?");
-  if (playAgain) {
-    playerScore = 0;
-    computerScore = 0;
-    playGame();
-  } else {
-    alert("Thanks for playing :)");
-    window.close();
-  }
-}
-
-function showPlayerLose(humanChoice, computerChoice) {
-  console.log("Nice try! User loses!");
-  console.log(
-    "user choice was: " +
-      humanChoice +
-      " " +
-      "computer choice was: " +
-      computerChoice
-  );
-}
-
-function showPlayerWin(humanChoice, computerChoice) {
-  console.log("Congrats! User wins!");
-  console.log(
-    "user choice was: " +
-      humanChoice +
-      " " +
-      "computer choice was: " +
-      computerChoice
-  );
-}
-
-function showGameOver() {
-  console.log("Game Over!");
-  if (playerScore === computerScore) {
-    console.log(`Wow! Both players tied!`);
-    console.log(`User score ${playerScore} : ${computerScore} Computer score`);
-  } else if (computerScore > playerScore) {
-    console.log(`Darn. Computer wins the game!`);
-    console.log(`User score ${playerScore} : ${computerScore} Computer score`);
-  } else {
-    console.log(`Nice! User wins the game!`);
-    console.log(`User score ${playerScore} : ${computerScore} Computer score`);
-  }
+  scoreNums[0].textContent = playerScore;
+  scoreNums[1].textContent = roundNum;
+  scoreNums[2].textContent = computerScore;
 }
 
 function getComputerChoice() {
@@ -71,58 +38,109 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Enter 'rock', 'paper', or 'scissors'!");
-  if (humanChoice) {
-    humanChoice = humanChoice.trim().toLowerCase();
-    return humanChoice;
-  }
-}
-
 function playRound(humanChoice, computerChoice) {
+  roundNum++;
   // Tie round
   if (humanChoice === computerChoice) {
-    alert("Tie! No points awarded");
-    console.log(
-      "player choice was: " +
-        humanChoice +
-        " " +
-        "computer choice was: " +
-        computerChoice
-    );
+    tieCount++;
+    result = "tie";
   } else if (
     // Player Wins
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
-    showPlayerWin(humanChoice, computerChoice);
     playerScore++;
-    showScoreUpdate();
+    result = "playerWins";
   } else if (
     // Player loses
     (humanChoice === "scissors" && computerChoice === "rock") ||
     (humanChoice === "rock" && computerChoice === "paper") ||
     (humanChoice === "paper" && computerChoice === "scissors")
   ) {
-    showPlayerLose(humanChoice, computerChoice);
     computerScore++;
-    showScoreUpdate();
+    result = "computerWins";
+  }
+  updateGameResult(result);
+  showScoreUpdate();
+
+  if (roundNum === 5) {
+    announceWinner(playerScore, computerScore);
+    endGame();
   }
 }
 
-function playGame() {
-  for (let round = 1; round <= 5; round++) {
-    console.log(`Round # ${round}`);
-    const humanSelection = getHumanChoice();
-    if (humanSelection === undefined) {
-      return;
-    }
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-  showGameOver();
-  resetGame();
+for (const btn of gameButtons) {
+  btn.addEventListener("click", () => {
+    humanChoice = btn.value;
+    humanChoice = humanChoice.toLowerCase();
+
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  });
 }
 
-playGame();
+let updateGameResult = (result) => {
+  if (result === "tie") {
+    gameResultNums[0].textContent = `Round ${roundNum} is a tie! Both players chose ${humanChoice}. Tie counter is at ${tieCount}.`;
+  } else if (result === "playerWins") {
+    gameResultNums[0].textContent = `Player wins round ${roundNum}! Player chose ${humanChoice} and Computer chose ${computerChoice}.`;
+    scoreNums[0].parentElement.classList.add("round-winner");
+    setTimeout(() => {
+      scoreNums[0].parentElement.classList.remove("round-winner");
+    }, 3000);
+  } else if (result === "computerWins") {
+    gameResultNums[0].textContent = `Computer wins round ${roundNum}! Player chose ${humanChoice} and Computer chose ${computerChoice}.`;
+    scoreNums[2].parentElement.classList.add("round-winner");
+    setTimeout(() => {
+      scoreNums[2].parentElement.classList.remove("round-winner");
+    }, 3000);
+  }
+};
+
+function announceWinner(playerScore, computerScore) {
+  if (playerScore === computerScore) {
+    matchResultText.textContent = `Game Over! Both players tied!`;
+  } else if (playerScore > computerScore) {
+    matchResultText.textContent = `Game Over! Player defeated the computer.`;
+    matchResultText.classList.add("game-win");
+  } else {
+    matchResultText.textContent = `Game Over! Computer defeated the player.`;
+    matchResultText.classList.add("game-lose");
+  }
+}
+
+function endGame() {
+  for (const btn of gameButtons) {
+    btn.style.display = "none";
+  }
+
+  const resetBtn = document.createElement("button");
+  resetBtn.id = "reset-btn";
+  resetBtn.classList.add("reset-btn");
+  resetBtn.textContent = "PLAY AGAIN";
+  resetBtn.addEventListener("click", resetGame);
+  gameButtonsDiv.appendChild(resetBtn);
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  roundNum = 0;
+  tieCount = 0;
+
+  for (const btn of gameButtons) {
+    btn.style.display = "";
+  }
+  document.querySelector("#reset-btn").remove();
+
+  matchResultText.textContent = "";
+  if (matchResultText.classList.length > 1) {
+    matchResultText.classList.remove("game-win");
+    matchResultText.classList.remove("game-lose");
+  }
+  gameResultNums[0].textContent = "";
+  scoreNums[0].textContent = "0";
+  scoreNums[1].textContent = "0";
+  scoreNums[2].textContent = "0";
+}
